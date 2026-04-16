bd0357a97d4a9126bb9f767a94aac5337c52ff42
infraflakes <infraflakes@proton.me>
2026-04-16 13:08:09 +0700

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -1,29 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
   };
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-master,
   }: let
     system = "x86_64-linux"; # or "aarch64-darwin" for M1/M2 Macs
     pkgs = import nixpkgs {inherit system;};
-    master-pkgs = import nixpkgs-master {inherit system;};
   in {
     devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [
-        master-pkgs.go_1_26
-        master-pkgs.golangci-lint
-        pkgs.gnumake
-        pkgs.bun
-        pkgs.biome
+      buildInputs = with pkgs; [
+        go_1_26
+        golangci-lint
+        gnumake
+        bun
+        biome
       ];
       shellHook = ''
-        go env -w GOPATH=$HOME/.local/share/go
+        export GOPATH="$HOME/.local/share/go"
+        export GOBIN="$GOPATH/bin"
+        export PATH="$GOBIN:$PATH"
         export PATH="$HOME/.local/bin:$PATH"
-        export PATH="$HOME/.local/share/go/bin:$PATH"
+
         export BUN_INSTALL="$HOME/.local/share/bun"
         export PATH="$BUN_INSTALL/bin:$PATH"
         export BUN_INSTALL_CACHE_DIR="$HOME/.cache/bun"
